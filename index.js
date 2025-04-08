0a4fa91d53f5fab0487e965ed052a1b9df7aee73
aman anurag <amananurag.20@gmail.com>
2025-04-08 11:40:53 +0530

    data recieved

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/products", function (req, res) {
   console.log("route hit hua");
+  console.log(req.query);
   res.send("<h1>get request product</h1>");
 });
 
 app.post("/products", function (req, res) {
   console.log("product route hit hua");
+  console.log(req.body);
   res.send("post product ka data hai");
 });
 
@@ -19,6 +23,11 @@ app.put("/products", function (req, res) {
 app.delete("/products", function (req, res) {
   console.log("product route hit hua");
   res.send("product ka data hai");
+});
+
+app.get("/products/:id", (req, res) => {
+  console.log(req.params);
+  res.send("this is product info");
 });
 
 app.listen(5000, function () {
