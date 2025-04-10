339d6fee9e64ffb872e286bb1cd767d13acb4d1f
aman anurag <amananurag.20@gmail.com>
2025-04-10 11:27:29 +0530

    added mongodb connection

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json()); //json->js object
-app.use(express.urlencoded({ extended: false })); //parse form data
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://amananurag20:j4DgBaxPkIrFOQcu@cluster0.dol4im4.mongodb.net/"
+    );
+    console.log("mongodb connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+connectDb();
+// mongodb+srv://amananurag20:j4DgBaxPkIrFOQcu@cluster0.dol4im4.mongodb.net/
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/products", function (req, res) {
-  console.log("route hit hua");
-  return res.send("hi dear");
-  console.log("hi how are you");
-});
-
-app.post("/products", function (req, res) {
-  console.log("product route hit hua");
-  console.log(req.body);
-  res.send("post product ka data hai");
-});
-
-app.put("/products", function (req, res) {
-  console.log("product route hit hua");
-  res.send("product ka data hai");
-});
-
-app.delete("/products", function (req, res) {
-  console.log("product route hit hua");
-  res.send("product ka data hai");
-});
-
-app.get("/products/:name", (req, res) => {
-  let name = req.params.name;
-  console.log(req.query);
-
-  res.send(`<h1>Welcome ${name}</h1>`);
+  res.send("<h1>This is highly credential data</h1>");
 });
 
 app.listen(5000, function () {
