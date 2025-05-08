b13175a2a90e592eef0d79d1bacee662bc501242
aman anurag <amananurag.20@gmail.com>
2025-05-08 11:10:11 +0530

    error handling middleware

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-const https = require("https");
-const crypto = require("crypto");
-const fs = require("fs");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRouter = require("./routes/userRoutes");
+const connectDb = require("./config/dbConnection");
+const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
-console.log("hello world");
 
-process.env.UV_THREADPOOL_SIZE = 6;
-var a = 1;
-var b = 2;
+connectDb();
 
-// Track time for each hash
-const start1 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end1 = Date.now();
-  console.log("hash- 1", `${end1 - start1}ms`);
+app.use(
+  cors({
+    origin: ["http://localhost:5174"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/users", userRouter);
+
+// app.use((req, res, next) => {
+//   console.log("hello");
+//   next();
+// });
+
+app.get("/product", (req, res, next) => {
+  // a;
+
+  try {
+    throw new Error("something is not working");
+  } catch (e) {
+    next(e);
+    // res.status(502).json({ message: "not working ......." });
+  }
 });
 
-const start2 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end2 = Date.now();
-  console.log("hash- 2", `${end2 - start2}ms`);
+app.use((err, req, res, next) => {
+  console.log("error handling middleware");
+  console.log(err);
+  res.send("page not found");
 });
 
-const start3 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end3 = Date.now();
-  console.log("hash- 3", `${end3 - start3}ms`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  console.log("server is running on port 5000");
 });
-
-const start4 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end4 = Date.now();
-  console.log("hash- 4", `${end4 - start4}ms`);
-});
-
-const start5 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end5 = Date.now();
-  console.log("hash- 5", `${end5 - start5}ms`);
-});
-
-const start6 = Date.now();
-crypto.pbkdf2("12345", "salt", 1000000, 64, "sha512", (err, data) => {
-  const end6 = Date.now();
-  console.log("hash- 6", `${end6 - start6}ms`);
-});
-
-// HTTPS request (handled by OS, not thread pool)
-https.get("https://jsonplaceholder.typicode.com/posts", (res) => {
-  console.log("data mil gaya");
-});
-
-// Simple sync function
-function add(a, b) {
-  console.log(a + b);
-}
-add(1, 2);
